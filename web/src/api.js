030565d3b2c71bd1ca5466e3457f5a42b9884239
Mohit Goyal <agrawal123456k@gmail.com>
2026-09-25 18:08:50 +0530

    Connect frontend to production backend

--- web/src/api.js
+++ web/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://studymate-4tj1.onrender.com";
 
 export async function testBackend() {
   const response = await fetch(`${API_URL}/api/test`);
